ea6cb1b910e0e3ae6b7378a16699d1870a1e8d58
Woo Jun Hyeok <junhyeok@torpedo.co.kr>
2023-03-09 17:00:18 +0900

    fix : ejs 추가

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/public',express.stats('public'))
+app.use('/public',express.static('public')) 
 
 app.set('view engine', 'ejs');
 
@@ -22,11 +22,11 @@ MongoClient.connect('mongodb+srv://wnsgur1569:qwaszx45@cluster0.foft3t5.mongodb.
 })
 
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.html');
+  response.render('index.ejs');
 });
 
 app.get('/write', (request, response) => {
-  response.sendFile(__dirname + '/write.html');
+  response.render('write.ejs');
 });
 
 app.get('/list',(request, response)=>{
